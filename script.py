3b8a8b102d0f87c99e21e3db9887af76b0479a55
IkeAlmighty <IsaacYates7@gmail.com>
2020-11-30 14:57:01 -0600

    test

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+import os
+
+screen = "mc"
+
+def do_command(screen, command):
+    os.system('screen -S {} -p 0 -X stuff "{}\n"'.format(screen, command))
 
 def execute():
+    do_command('say hello this is a test')
     pass
