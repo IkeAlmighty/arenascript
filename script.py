8903094c3d8ebb33f0bda2fc165d2f8a78f5ea63
IkeAlmighty <IsaacYates7@gmail.com>
2020-11-30 16:15:59 -0600

    NONE

--- script.py
+++ script.py
@@ -24,24 +24,44 @@ rooms = {
 
 ## CUSTOM FUNCTIONS ##
 
-def lock_room():
-    pass
+def lock_room(room_name):
+    # get the room:
+    room = rooms[room_name]
+    # first, figure out where each doorway is:
+    x, y, z = room[0], room[1], room[2]
+    doors = [
+        (x + 4, y, z), 
+        (x - 4, y, z), 
+        (x, y, z + 4), 
+        (x, y, z - 4)
+    ]
+
+    # then, for each door, fill it!
+    for door in doors:
+        x, y, z = door[0], door[1], door[2]
+        do_command("fill {} {} {} {} {} {} minecraft:obsidian".format(x, y, z, x, y + 4, z))
 
 # for randl 'random list element'
 def randl(_list):
     return _list[random.randint(0, len(_list) - 1)]
 
+# TODO: this is not working because of quotations or something
 def item_to_room(room_name, item, count):
     room = rooms[room_name]
     cmd_string = "summon minecraft:item {} {} {} {{Item:{{id:\"minecraft:{}\",Count:{}b}}}}"
+    cmd = cmd_string.format(room[0], room[1], room[2], item, count)
+    print(cmd)
+    # os.system("echo {}".format(cmd))
     do_command(cmd_string.format(room[0], room[1], room[2], item, str(count)))
+
+item_to_room("YELLOW", "stone_sword", 2)
 
 
 ## MAIN LOOP ##
 def execute():
     do_command('give @a minecraft:{}'.format(randl(start_weapons)))
     do_command('give @a minecraft:{}'.format(randl(start_food)))
-
+    lock_room("CENTER")
     while True:
         # do_command('give @a minecraft:golden_apple')
         item_to_room("CENTER", "coal", 5)
